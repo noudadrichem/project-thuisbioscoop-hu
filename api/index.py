1d4d3960aec8100aac149d5efb1a527f9d2955b0
info@noudadrichem.com <info@noudadrichem.com>
2018-10-14 12:47:41 +0200

    api → movie details

---
+++ api/index.py
@@ -0,0 +1,25 @@
+import pprint
+import requests
+import xmltodict
+
+
+uniqueApiId = 'u2urjstlwm7yb4qrkts35wxufnvynim2'
+api_url =  'http://api.filmtotaal.nl/filmsoptv.xml?apikey=u2urjstlwm7yb4qrkts35wxufnvynim2&dag=14-10-2018&sorteer=0'
+
+r = requests.get(api_url)
+# print(r.status_code)
+
+
+text = r.text.encode('ascii', 'ignore')
+
+isXML = r.headers['content-type'] == 'text/xml'
+if isXML:
+    moviesDic = xmltodict.parse(text)
+    for film in moviesDic['filmsoptv']['film']:
+
+        title = film['titel']
+        regiseur = film['regisseur']
+        description = film['synopsis']
+        imdbScore = film['imdb_rating']
+        print(title)
+
