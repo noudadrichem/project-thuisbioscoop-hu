aba6123dbbde16e685a9a1ed1252ed41657449f2
Dneuteboom <43525599+Dneuteboom@users.noreply.github.com>
2018-10-15 19:55:34 +0200

    Add files via upload

---
+++ user/qr-popup.py
@@ -0,0 +1,33 @@
+# install deze packeges zodat ie 100% werkt.
+#image package
+#pillow package
+
+
+#qr code met random key.
+
+def QR(username,film_naam):
+    import qrcode
+    import uuid
+
+    code = (uuid.uuid4())
+    img = qrcode.make(code)
+    a = ('{}-{}.png'.format(username,film_naam))
+    img.save(a,'PNG')
+    kijken = open(a)
+    return kijken
+QR(username,film_naam)
+
+
+
+#Qr code die alleen naam + username aangeeft
+
+# def QR_filmnaam(film_naam,username):
+#     import qrcode
+
+#     code = film_naam
+#     img = qrcode.make(film_naam)
+#     a = ('{}_{}.png'.format(film_naam,username))
+#     img.save(a,'PNG')
+#     kijken = open(a)
+#     return kijken
+# QR_filmnaam('Harry snotter', 'herndia')
