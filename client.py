bba97e1482afe3c938a84ec309c973938fc9562a
info@noudadrichem.com <info@noudadrichem.com>
2018-10-15 17:17:57 +0200

    basic gui

--- client.py
+++ client.py
@@ -1,5 +1,29 @@
 from api import movies, singleMovie
+from tkinter import *
 
-movies = movies(False)
+movies = movies()
 single = singleMovie(movies[0]['title'])
 
+
+root = Tk()
+root.configure(background='#f5f5f5')
+
+def movieLabel(movieTitle):
+
+    label = Label(
+        master=root,
+        text=movieTitle,
+        height=2,
+    )
+    label.pack()
+    return label
+
+
+for movie in movies:
+    movieLabel(movie['title'])
+    root.update()
+
+button = Button(master=root, text='Druk hier')
+button.pack(pady=10)
+
+root.mainloop()
