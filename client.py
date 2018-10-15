579e1bbf08223ce840a5ab888eeb16ceef367814
info@noudadrichem.com <info@noudadrichem.com>
2018-10-15 18:16:07 +0200

    overview ready

--- client.py
+++ client.py
@@ -5,25 +5,45 @@ movies = movies()
 single = singleMovie(movies[0]['title'])
 
 
+bg = '#f5f5f5'
 root = Tk()
-root.configure(background='#f5f5f5')
+root.configure(background=bg,)
 
-def movieLabel(movieTitle):
+# root.geometry('{}x{}'.format(500, 300))
 
+label = Label(
+    master=root,
+    text='FILMS VANDAAG',
+    height=2,
+    justify=LEFT,
+    background=bg,
+    font=("Open Sans", 12, "bold")
+)
+label.grid(row=0, columnspan=2, sticky='w')
+
+def movieLabel(movieTitle, idx):
     label = Label(
         master=root,
         text=movieTitle,
         height=2,
+        justify=LEFT,
+        background=bg,
+        font=("Open Sans", 12)
     )
-    label.pack()
-    return label
+    label.grid(row=idx+1, column=1, sticky='w')
+
+    button = Button(master=root, text='Aanmelden')
+    button.grid(row=idx+1, column=2)
+    return label, button
 
 
-for movie in movies:
-    movieLabel(movie['title'])
-    root.update()
+for idx in range(len(movies)):
+    movie = movies[idx]
+    movieLabel(movie['title'], idx)
 
-button = Button(master=root, text='Druk hier')
-button.pack(pady=10)
+
+root.update()
+
+
 
 root.mainloop()
