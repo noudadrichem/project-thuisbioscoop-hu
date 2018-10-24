830a5eff89d28e89095c251552ad8a455fffec19
info@noudadrichem.com <info@noudadrichem.com>
2018-10-24 14:57:32 +0200

    alles weer gelijk

--- client.py
+++ client.py
@@ -10,12 +10,14 @@ from datetime import datetime
 
 moviesVandaag = movies(True)
 moviesMorgen = movies(False)
+tableHeadings = ['Film titel', 'Aanbieder', 'Start tijd']
+beheerders = ['Maarten', 'Daan', 'Noud', 'Pascal']
 bg = '#f5f5f5'
 
 def formattedUnix(time):
     return datetime.utcfromtimestamp(int(time)).strftime('%H:%M')
 
-def lefAlignedLabel(title, idx, window, columnNumber, bold=False):
+def lefAlignedLabel(title, counter, window, columnNumber, bold=False):
     label = Label(
         master=window,
         text=title,
@@ -24,8 +26,33 @@ def lefAlignedLabel(title, idx, window, columnNumber, bold=False):
         background=bg,
         font=("Open Sans", 12, 'bold' if bold else 'normal')
     )
-    label.grid(row=idx, column=columnNumber, sticky='w')
+    label.grid(row=counter, column=columnNumber, sticky='w')
     return label
+
+
+
+def tableHeading(root, index, text):
+    label3 = Label(
+        master=root,
+        text=text,
+        height=2,
+        justify=LEFT,
+        background=bg,
+        font=("Open Sans", 8, "bold")
+    )
+    label3.grid(row=1, column=index, columnspan=2, sticky='w')
+
+    label4 = Label(
+        master=root,
+        text=text,
+        height=2,
+        justify=LEFT,
+        background=bg,
+        font=("Open Sans", 8, "bold")
+    )
+    label4.grid(row=1, column=index+6, columnspan=2, sticky='w')
+
+
 
 
 def movieLabel(movie, idx, window, columnNumber):
@@ -35,7 +62,16 @@ def movieLabel(movie, idx, window, columnNumber):
         label = labels[jdx]
         lefAlignedLabel(label, (idx+1), window, (columnNumber + jdx))
 
-    button = Button(master=window, text='Aanmelden', command= lambda: popupSignUp(idx, window, movieTitle, aanbieder))
+    button = Button(
+        master=window, 
+        text='Aanmelden', 
+        command= lambda: popupSignUp(
+            idx,
+            window,
+            movie['title'],
+            movie['aanbieder']
+            )
+        )
     button.grid(row=(idx+1), column=columnNumber+4)
 
 
@@ -143,6 +179,26 @@ def wrongUserPopUp():
     return popup
 
 
+def overzichtbeheerders():
+    root = Tk()
+    root.configure(background=bg)
+    root.title('Overzicht beheerders')
+    label1 = Label(
+        master=root,
+        text='Wie ben jij?',
+        height=2,
+        justify=LEFT,
+        background=bg,
+        font=("Open Sans", 12, "bold"))
+    label1.grid(row=0, column=1, columnspan=2)
+    counter = 1
+    for beheerder in beheerders:
+        counter = counter + 1
+        button = Button(master=root, width=50, text=beheerder)#, command= lambda: behheerdersaccount()
+        button.grid(row=(counter), column= 1)
+
+
+
 def client():
     root = Tk()
     root.configure(background=bg)
@@ -163,14 +219,27 @@ def client():
         height=2,
         justify=LEFT,
         background=bg,
-        font=("Open Sans", 12, "bold"))
+        font=("Open Sans", 12, "bold")
+    )
     label2.grid(row=0, column=6, columnspan=2, sticky='w')
 
+    beheerderbutton = Button(master=root, text='ben jij een beheerder?', command=lambda: overzichtbeheerders())
+    beheerderbutton.grid(row=len(moviesVandaag)+4, column=1)
+
+    for index in range(len(tableHeadings)):
+        heading = tableHeadings[index]
+        tableHeading(root, index+1, heading)
+
+
+    vandaagcounter = 2
+    morgencounter = 2
     for idx in range(len(moviesVandaag)):
-        movieLabel(moviesVandaag[idx], idx, root, 1)
+        vandaagcounter = vandaagcounter + 1
+        movieLabel(moviesVandaag[idx], vandaagcounter, root, 1)
 
     for jdx in range(len(moviesMorgen)):
-        movieLabel(moviesMorgen[jdx], jdx, root, 7)
+        morgencounter = morgencounter + 1
+        movieLabel(moviesMorgen[jdx], morgencounter, root, 7)
 
     root.update()
     root.mainloop()
